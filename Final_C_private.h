c7784834450ba5e4b1019edf0e4e1e6a6440b45c
kity-linuxero <cgiambruni@gmail.com>
2016-09-30 12:13:06 -0300

    Correcciones en textos de versionado en el IDE

--- Final_C_private.h
+++ Final_C_private.h
@@ -5,16 +5,16 @@
 #define FINAL_C_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.1.1.2943"
+#define VER_STRING	"0.1.1.2953"
 #define VER_MAJOR	0
 #define VER_MINOR	1
 #define VER_RELEASE	1
-#define VER_BUILD	2943
+#define VER_BUILD	2953
 #define COMPANY_NAME	""
-#define FILE_VERSION	"0.1.1.2943"
-#define FILE_DESCRIPTION	"Para Facultad de Informática"
+#define FILE_VERSION	"0.1.1.2953"
+#define FILE_DESCRIPTION	"Combinador de grupo40"
 #define INTERNAL_NAME	""
-#define LEGAL_COPYRIGHT	""
+#define LEGAL_COPYRIGHT	"(c) 2006- Apache Licence 2.0"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"Combinador"
